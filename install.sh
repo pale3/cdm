8744bb0d2ef5f8ee419775ff921e31bb43b10559
Marko Rakamaric <marko.rakamaric@helmholz.de>
2016-08-17 16:03:10 +0200

    Adjust install locations for forcefull manual instalation

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 install -d /usr/bin /etc /usr/share/doc/cdm/themes
+install -Dm755 src/cdm /usr/bin
 install -Dm644 src/cdmrc /etc/cdmrc
-install -Dm644 README.md src/profile.sh /usr/share/doc/cdm
+install -Dm644 src/profile.sh /etc/profile.d
+install -Dm644 README.md /usr/share/doc/cdm
 install -Dm644 themes/* /usr/share/doc/cdm/themes
-
